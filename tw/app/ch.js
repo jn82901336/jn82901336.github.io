76de081a978844ca467279fb4dcc13f633753aa0
rrd <>
2021-03-13 17:30:02 +0100

    Update

--- tw/app/ch.js
+++ tw/app/ch.js
@@ -145,7 +145,7 @@ var ockovaniT;
 var chart;
 var data={};
 var vakciny={}
-var default_colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC'] ;
+var default_colors = ['#3366CC','#DC3912','#109618','#3B3EAC','#990099','#0099C6','#DD4477','#FF9900','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC'] ;
 $(function () {
  $.getJSON('data/prijemT.min.json', addPrijem);
  $('tr[id^=CZ]').click(function(i){graf($(this).attr('id'));});
